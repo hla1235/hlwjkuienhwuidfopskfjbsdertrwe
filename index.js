5fdc4d69265e9c784feb4c80b86b453dfd8ae828
hla1235 <43097192+hla1235@users.noreply.github.com>
2018-09-08 21:52:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var Discord = require("discord.js");
 
-var bot = new Discord.Client();
+var client = new Discord.Client();
 
 const config = require("./config.json");
 
 var load = require("./src/load");
-load(bot, config);
+load(client, config);
 
 var track = require("./src/track");
-track(bot, config);
+track(client, config);
 
-bot.login(require("./token.json"))
+client.login(require("./token.json"))
 	.then(() => {
-		console.log(`Logged in as ${bot.user.tag}`);
+		console.log(`Logged in as ${client.user.tag}`);
 	})
 	.catch(error => {
 		console.error(error);
