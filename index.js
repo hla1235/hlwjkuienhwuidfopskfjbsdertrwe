003c39194e81c40eca6bd8e7369aeb5539b3bb1a
hla1235 <43097192+hla1235@users.noreply.github.com>
2018-09-08 21:14:36 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+const config = require("./config.json");
+
+var load = require("./src/load");
+load(bot, config);
+
+var track = require("./src/track");
+track(bot, config);
+
+bot.login(require("./token.json"))
+	.then(() => {
+		console.log(`Logged in as ${bot.user.tag}`);
+	})
+	.catch(error => {
+		console.error(error);
+	});
